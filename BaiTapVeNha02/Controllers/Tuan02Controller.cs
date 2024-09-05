dc8cf6f00c4d579d5f66b78b9d75d9ae7cc5c757
zakiet <zakiet2004@gmail.com>
2024-09-05 17:41:33 +0700

    hoan thanh

---
+++ BaiTapVeNha02/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha02.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Lam Gia Kiet";
+            ViewBag.MSSV = "1822041104";
+            ViewBag.Nam = "Nam 2024";
+            return View();
+        }
+    }
+}
